b789c102f58aa0ea71b42c9a4eadd0c8cafb99c8
itunz1 <stx_jose@hotmail.com>
2023-11-24 18:06:45 -0400

    guide feature components

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Button from "./Button"
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-20 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
       <div className="hero-map" />
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <Image src="/camp.svg" alt="camp" width={50} height={50} className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]" />
@@ -18,7 +18,7 @@ const Hero = () => {
         </p>
 
         <div className="my-11 flex flex-wrap gap-5">
-          <div className="flex itemx-center gap-2">
+          <div className="flex items-center gap-2">
             {Array(5).fill(1).map((_, index) => (
               <Image src="/star.svg" alt="star" key={index} width={24} height={24} />
             ))}
@@ -48,7 +48,7 @@ const Hero = () => {
           <div className="flexBetween">
             <div className="flex flex-col">
               <p className="regular-16 block text-gray-20">Distance</p>
-              <p className="bold-20 text-white">173.28mi</p>
+              <p className="bold-20 text-white">173.28 mi</p>
             </div>
             <div className="flex flex-col">
               <p className="regular-16 block text-gray-20">Elevation</p>
